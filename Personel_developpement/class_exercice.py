b63058388fba7a6f3309a213a52cf30c86689b73
Loris MARTINEZ <loris.martinez78500@gmail.com>
2025-04-18 22:30:31 +0200

    remettre a demain

--- Personel_developpement/class_exercice.py
+++ Personel_developpement/class_exercice.py
@@ -1,25 +1,28 @@
 class Player:
-    def __init__ (self,name,level):
-        self.name = name
+    def __init__ (self):
+        self.name = input("Entrez le nom de votre personnage ")
         self.xp = 0
-        self.level = level
+        self.level = 1
 
 
     def presentation(self):
-        print(f"I'm the player {self.name} and I'm level {self.level}")
+        print(f"I'm the player {self.name} and I'm level {self.level}.")
+        print(f"for the moment i only have {self.xp} pts of XP but in ")
 
-level = 1
+    def gain_xp(self,amount):
+        self.xp = self.xp + amount
+        print(f"You earned {amount} pts of xp")
+        if self.xp >= 
 
-player1 = Player(name = "loris", level = level)
+player1 = Player()
+player1.presentation()
 
-xp = 0
-x = 0
-while xp > 0 :
+
+
+while player1.xp >= 0 :
+    x = player1.level - 1
     player1.level = player1.level + 1
-    xp = xp - (500 * (1.2) ** x)
+    player1.xp = player1.xp - (500 * (1.2) ** x)
     x = x+1
-    print(f"{xp}")
+    print(f"{player1.xp}")
     player1.presentation()
-
-print("the fuck")
-
